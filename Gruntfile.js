f781134a00e15a85fbb2971a5ce237d6aea7b0da
hugohua <baofen14787@gmail.com>
2014-04-23 15:45:31 +0800

    新增bower 及 Grunt 来管理插件

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,14 +6,27 @@ module.exports = function(grunt){
         uglify: {
             //文件头部输出信息
             options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                banner: '/*\n' +
+                    ' * <%= pkg.name %> <%= pkg.version %>\n' +
+                    ' * <%= pkg.description %>\n' +
+                    ' *\n' +
+                    ' * <%= pkg.homepage %>\n' +
+                    ' *\n' +
+                    ' * Copyright <%= grunt.template.today("yyyy") %>, <%= pkg.author %>\n' +
+                    ' * The iDangero.us\n' +
+                    ' * http://www.idangero.us/\n' +
+                    ' *\n' +
+                    ' * Licensed under <%= pkg.license.join(" & ") %>\n' +
+                    ' *\n' +
+                    ' * Released on: <%= grunt.template.today("mmmm d, yyyy") %>\n' +
+                    '*/\n'
             },
             //具体任务配置
             build: {
                 //源文件
-                src: 'js/zepto.snapscroll.js',
+                src: 'zepto.snapscroll.js',
                 //目标文件
-                dest: 'js/zepto.snapscroll-min.js'
+                dest: 'zepto.snapscroll.min.js'
             }
         }
     });
